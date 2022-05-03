b355690bef24a141b468f38bec680d01ef3a3e83
jawale204 <jawale204@gmail.com>
2022-05-03 12:34:09 +0530

    products displayed

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,18 +1,35 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 function ProductComponent() {
-  return (
-    <div className="four wide column">
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image"></div>
-          <div className="content">
-            <div className="header"></div>
-            <div className="meta price"></div>
-            <div className="meta"></div>
-          </div>
+  const products = useSelector((state) => state.allProducts.products);
+  return products.map((product) => {
+    const { id, image, title, category, price } = product;
+    return (
+      <>
+        <div className="four wide column" key={id}>
+          <Link to={`/product/${id}`}>
+            <div className="ui link cards">
+              <div className="card">
+                <img
+                  src={image}
+                  alt={title}
+                  width="250px"
+                  height="250px"
+                  className="image"
+                />
+                <div className="content">
+                  <div className="header">{title}</div>
+                  <div className="meta price">$ {price}</div>
+                  <div className="meta">{category}</div>
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  });
 }
 
 export default ProductComponent;
